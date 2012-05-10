df31f4ef5106413eda6d1fb0f40ed616af441018
Marv Cool <marv@hostin.is>
2012-05-10 16:54:25 +0200

    fixed indention

--- lib/tsm-accounting.rb
+++ lib/tsm-accounting.rb
@@ -64,33 +64,33 @@ module TSMAccounting
     end # initialize
 
     def to_csv(output_file)
-		CSV.open(output_file, 'w') do |f|
-			f << ['Realm','Faction','Transaction Type','Time','Item ID','Item Name','Quantity','Stack Size','Price (g)','Price (c)','Buyer','Seller']
-			@data.each do |realm,factions|
-				factions.each do |faction,ropes|
-					ropes.each do |type,items|
-						unless items.nil?
-							items.each do |name,item|
-								item.transactions.each do |tx|
-									row = [realm,faction,type] 
-									row << tx.datetime.strftime('%Y-%m-%d %k:%M:%S')
-									row << item.id
-									row << item.name
-									row << tx.quantity
-									row << tx.stack_size
-									row << tx.usable_price
-									row << tx.price
-									row << tx.buyer
-									row << tx.seller
-									f << row
-								end			  
-							end
-						end # check for emtpy items end
-					end 
-				end
-			end
-		end # close CSV
-    end # to_csv
+      CSV.open(output_file, 'w') do |f|
+        f << ['Realm','Faction','Transaction Type','Time','Item ID','Item Name','Quantity','Stack Size','Price (g)','Price (c)','Buyer','Seller']
+        @data.each do |realm,factions|
+          factions.each do |faction,ropes|
+            ropes.each do |type,items|
+              unless items.nil?
+                items.each do |name,item|
+                  item.transactions.each do |tx|
+                    row = [realm,faction,type] 
+                    row << tx.datetime.strftime('%Y-%m-%d %k:%M:%S')
+                    row << item.id
+                    row << item.name
+                    row << tx.quantity
+                    row << tx.stack_size
+                    row << tx.usable_price
+                    row << tx.price
+                    row << tx.buyer
+                    row << tx.seller
+                    f << row
+                  end
+                end
+              end # check for emtpy items end                 
+            end                           
+          end                 
+        end 
+      end # close CSV   
+    end # to_csv        
 
 
     def parse_rope(rope,type)
